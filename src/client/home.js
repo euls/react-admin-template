62cb552c8ee48cad6fa016d870db5c9a42ebf1ea
Shawn Lee <lhn1028@163.com>
2017-12-06 16:59:09 +0800

    home-app -> admin，加入react-router-dom

--- src/client/home.js
+++ src/client/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import fetch from 'isomorphic-fetch';
-import HomeApp from './pages/home-app';
+import AdminApp from './pages/admin';
 import './less/home.less';
 
-render(<HomeApp/>, document.getElementById('app'));
+render(<AdminApp/>, document.getElementById('app'));
