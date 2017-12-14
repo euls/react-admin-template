b9390c64fbfebd27f51eaa6c3154664319be8b4f
Shawn Lee <lhn1028@163.com>
2017-12-14 11:39:38 +0800

    配置browser-sync，自动更新hbs及hbs的样式

--- scripts/startup.dev.js
+++ scripts/startup.dev.js
@@ -21,7 +21,7 @@ const watchOptions = {
     // Uncomment next line if it is your case (use true or interval in milliseconds)
     // poll: true,
     // Decrease CPU or memory usage in some file systems
-    // ignored: /node_modules/,
+    ignored: /node_modules/,
 };
 
 let app;
@@ -89,124 +89,113 @@ async function start() {
         watchOptions
     }));
 
-    server.use(webpackDevMiddleware(serverCompiler, {
-        publicPath: serverConfig.output.publicPath,
-        quiet: true,
-        noInfo: true,
-        stats: {
-            chunks: false,
-            color: true
-        },
-        watchOptions
-    }));
-
     server.use(webpackHotMiddleware(clientCompiler, {
         log: false
     }));
 
-    // const clientPromise = createCompilationPromise(
-    //     'client',
-    //     clientCompiler,
-    //     clientConfig,
-    // );
-    // const serverPromise = createCompilationPromise(
-    //     'server',
-    //     serverCompiler,
-    //     serverConfig,
-    // );
+    const clientPromise = createCompilationPromise(
+        'client',
+        clientCompiler,
+        clientConfig,
+    );
+    const serverPromise = createCompilationPromise(
+        'server',
+        serverCompiler,
+        serverConfig,
+    );
 
-    // let appPromise;
-    // let appPromiseResolve;
-    // let appPromiseIsResolved = true;
-    // serverCompiler.plugin('compile', () => {
-    //     if (!appPromiseIsResolved) return;
-    //     appPromiseIsResolved = false;
-    //     appPromise = new Promise(resolve => (appPromiseResolve = resolve));
-    // });
+    let appPromise;
+    let appPromiseResolve;
+    let appPromiseIsResolved = true;
+    serverCompiler.plugin('compile', () => {
+        if (!appPromiseIsResolved) return;
+        appPromiseIsResolved = false;
+        appPromise = new Promise(resolve => (appPromiseResolve = resolve));
+    });
 
-    // server.use((req, res) => {
-    //     appPromise
-    //         .then(() => {
-    //             if (!fs.existsSync(path.join(process.cwd(), 'build/server/views'))) {
-    //                 ncp(path.join(process.cwd(), 'src/server/views'), path.join(process.cwd(), 'build/server/views'), err=>{
-    //                     if (err) {
-    //                         console.log(err);
-    //                     }
+    server.use((req, res) => {
+        appPromise
+            .then(() => {
+                if (!fs.existsSync(path.join(process.cwd(), 'build/server/views'))) {
+                    ncp(path.join(process.cwd(), 'src/server/views'), path.join(process.cwd(), 'build/server/views'), err=>{
+                        if (err) {
+                            console.log(err);
+                        }
 
-    //                     app.set('views', path.join(process.cwd(), 'build/server/views'));
-    //                     app.handle(req, res);
-    //                 });
-    //             } else {
-    //                 app.set('views', path.join(process.cwd(), 'build/server/views'));
-    //                 app.handle(req, res);
-    //             }
-    //         })
-    //         .catch(error => console.error(error));
-    // });
+                        app.set('views', path.join(process.cwd(), 'build/server/views'));
+                        app.handle(req, res);
+                    });
+                } else {
+                    app.set('views', path.join(process.cwd(), 'build/server/views'));
+                    app.handle(req, res);
+                }
+            })
+            .catch(error => console.error(error));
+    });
 
-    // function checkForUpdate(fromUpdate) {
-    //     const hmrPrefix = '[\x1b[35mHMR\x1b[0m] ';
-    //     if (!app.hot) {
-    //         throw new Error(`${hmrPrefix}Hot Module Replacement is disabled.`);
-    //     }
-    //     if (app.hot.status() !== 'idle') {
-    //         return Promise.resolve();
-    //     }
-    //     return app.hot
-    //         .check(true)
-    //         .then(updatedModules => {
-    //             if (!updatedModules) {
-    //                 if (fromUpdate) {
-    //                     console.info(`${hmrPrefix}Update applied.`);
-    //                 }
-    //                 return;
-    //             }
-    //             if (updatedModules.length === 0) {
-    //                 console.info(`${hmrPrefix}Nothing hot updated.`);
-    //             } else {
-    //                 console.info(`${hmrPrefix}Updated modules:`);
-    //                 updatedModules.forEach(moduleId =>
-    //                     console.info(`${hmrPrefix} - ${moduleId}`),
-    //                 );
-    //                 checkForUpdate(true);
-    //             }
-    //         })
-    //         .catch(error => {
-    //             if (['abort', 'fail'].includes(app.hot.status())) {
-    //                 console.warn(`${hmrPrefix}Cannot apply update.`);
-    //                 delete require.cache[entryPoint];
-    //                 // eslint-disable-next-line global-require, import/no-unresolved
-    //                 app = require(entryPoint).default;
-    //                 console.warn(`${hmrPrefix}App has been reloaded.`);
-    //             } else {
-    //                 console.warn(
-    //                     `${hmrPrefix}Update failed: ${error.stack || error.message}`,
-    //                 );
-    //             }
-    //         });
-    // }
+    function checkForUpdate(fromUpdate) {
+        const hmrPrefix = '[\x1b[35mHMR\x1b[0m] ';
+        if (!app.hot) {
+            throw new Error(`${hmrPrefix}Hot Module Replacement is disabled.`);
+        }
+        if (app.hot.status() !== 'idle') {
+            return Promise.resolve();
+        }
+        return app.hot
+            .check(true)
+            .then(updatedModules => {
+                if (!updatedModules) {
+                    if (fromUpdate) {
+                        console.info(`${hmrPrefix}Update applied.`);
+                    }
+                    return;
+                }
+                if (updatedModules.length === 0) {
+                    console.info(`${hmrPrefix}Nothing hot updated.`);
+                } else {
+                    console.info(`${hmrPrefix}Updated modules:`);
+                    updatedModules.forEach(moduleId =>
+                        console.info(`${hmrPrefix} - ${moduleId}`),
+                    );
+                    checkForUpdate(true);
+                }
+            })
+            .catch(error => {
+                if (['abort', 'fail'].includes(app.hot.status())) {
+                    console.warn(`${hmrPrefix}Cannot apply update.`);
+                    delete require.cache[entryPoint];
+                    // eslint-disable-next-line global-require, import/no-unresolved
+                    app = require(entryPoint).default;
+                    console.warn(`${hmrPrefix}App has been reloaded.`);
+                } else {
+                    console.warn(
+                        `${hmrPrefix}Update failed: ${error.stack || error.message}`,
+                    );
+                }
+            });
+    }
 
-    // serverCompiler.watch(watchOptions, (error, stats) => {
-    //     if (app && !error && !stats.hasErrors()) {
-    //         checkForUpdate().then(() => {
-    //             appPromiseIsResolved = true;
-    //             appPromiseResolve();
-    //         });
-    //     }
-    // });
+    serverCompiler.watch(watchOptions, (error, stats) => {
+        if (app && !error && !stats.hasErrors()) {
+            checkForUpdate().then(() => {
+                appPromiseIsResolved = true;
+                appPromiseResolve();
+            });
+        }
+    });
 
-    // await clientPromise;
-    // await serverPromise;
+    await clientPromise;
+    await serverPromise;
 
-    // const timeStart = new Date();
-    // console.info(`[${format(timeStart)}] Launching server...`);
+    const timeStart = new Date();
+    console.info(`[${format(timeStart)}] Launching server...`);
 
     // // Load compiled src/server.js as a middleware
     // // eslint-disable-next-line global-require, import/no-unresolved
-    // app = require('../build/server/server').default;
-    // app.set('views', path.join(process.cwd(), 'build/server/views'));
-    // appPromiseIsResolved = true;
-    // appPromiseResolve();
+    app = require('../build/server/server').default;
+    app.set('views', path.join(process.cwd(), 'build/server/views'));
+    appPromiseIsResolved = true;
+    appPromiseResolve();
 
     const port = process.env.PORT || 8080;
     const serverInstance = server.listen(port, () => {
@@ -215,7 +204,7 @@ async function start() {
             ui: false,
             notify: false,
             proxy: 'localhost:8080',
-            files: ['../src/server/views/**'],
+            files: ['build/server/views/**'],
             port: 3000
         });
         console.log(`Development server is listening on: http://localhost:${port}`);
